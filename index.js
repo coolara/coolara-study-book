ab527b3ee6c500efccd3c1d58cfb9446e590ddbd
coolara <772114880@qq.com>
2021-12-16 00:35:16 +0800

    update flag

--- index.js
+++ index.js
@@ -1,67 +1,102 @@
-class Heap {
+class MinHeap {
   constructor() {
     this.data = [];
-    this.cnt = 0;
   }
-  size() {
-    return this.cnt;
-  }
-  top() {
+  peek() {
     return this.data[0];
   }
-  //[1,2,3，4] 3
-  push(val) {
-    this.data[this.cnt++] = val;
-    this.liftUp(this.cnt - 1);
+  size() {
+    return this.data.length;
+  }
+  push(x) {
+    this.data.push(x);
+    this.liftUp(this.data.length - 1);
+  }
+  compare(i, j) {
+    return this.data[i] - this.data[j] < 0;
   }
   swap(i, j) {
     [this.data[i], this.data[j]] = [this.data[j], this.data[i]];
   }
   liftUp(ind) {
-    while (ind) {
-      if (this.data[ind] > this.data[(ind - 1) >> 1]) {
-        this.swap(ind, (ind - 1) >> 1);
-        ind = (ind - 1) >> 1;
+    let index = ind;
+    while (index > 0) {
+      let parentIndex = (index - 1) >> 1;
+      if (this.compare(index, parentIndex)) {
+        this.swap(index, parentIndex);
+        index = parentIndex;
+      } else {
+        break;
       }
     }
   }
   pop() {
-    if (this.size() == 0) return;
-    // this.data[0] = this.data.pop();
-    this.swap(0, this.cnt - 1);
-    this.cnt -= 1;
-    this.liftDown(0);
+    const last = this.data.pop();
+    if (this.size() != 0) {
+      this.data[0] = last;
+      this.liftDown(0);
+    }
   }
   liftDown(ind) {
-    let n = this.cnt - 1; // ind 父节点 怎么求子  ind * 2 +1
-    while (ind * 2 + 1 <= n) {
-      let temp = ind;
-      // 和左子节点 调整
-      if (this.data[temp] < this.data[ind * 2 + 1]) {
-        temp = ind * 2 + 1;
+    // 先和左比
+    let n = this.data.length;
+    let halfLen = n >> 1;
+    while (ind < halfLen) {
+      let left = (ind + 1) * 2 - 1;
+      let right = left + 1;
+      if (left < halfLen && this.compare(left, ind)) {
+        if (right < n && this.compare(right, left)) {
+          this.swap(ind, right);
+          ind = right;
+        } else {
+          this.swap(ind, left);
+          ind = left;
+        }
+      } else if (right < n && this.compare(right, ind)) {
+        this.swap(ind, right);
+        ind = right;
+      } else {
+        break;
       }
-      // 和右子节点 调整
-      if (ind * 2 + 2 <= n && this.data[temp] < this.data[ind * 2 + 2]) {
-        temp = ind * 2 + 2;
-      }
-      if (temp == ind) break;
-      this.swap(temp, ind);
-      ind = temp;
     }
   }
   print() {
     console.log(this.data);
   }
 }
+var KthLargest = function (k, nums) {
+  this.heap = new MinHeap();
+  this.k = k;
+  for (const num of nums) {
+    this.add(num);
+  }
+};
 
-let heap = new Heap();
-heap.push(3);
-heap.push(8);
-heap.push(12);
-heap.push(16);
-let len = heap.size() - 1;
-console.log(len);
-while (len--) {
-  heap.pop();
-}
-heap.print();
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function (val) {
+  this.heap.push(val);
+  if (this.heap.size() > this.k) {
+    this.heap.pop();
+  }
+  this.heap.print();
+  return this.heap.peek();
+};
+kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3); // return 4
+kthLargest.add(5); // return 5
+kthLargest.add(10); // return 5
+kthLargest.add(9); // return 8
+kthLargest.add(4); // return 8
+
+// [ 4 ]
+// [ 4, 5 ]
+// [ 4, 5, 8 ]
+// [ 4, 5, 8 ]
+// [ 4, 5, 8 ]
+// [ 5, 5, 8 ]
+// [ 5, 10, 8 ]
+// [ 8, 9, 10 ]
+// [ 8, 9, 10 ]
